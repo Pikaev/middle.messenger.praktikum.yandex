c7a37e3ad01f491d80c4bc37b7a77a08acbe1a3e
Sergey <yapikay@ya.ru>
2024-12-03 23:05:15 +0300

    Add test AuthPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import Handlebars from "handlebars";
 import * as Pages from "./pages";
 
+// Register partials
+import Input from "./components/Input.js";
+
+Handlebars.registerPartial("Input", Input);
 export default class App {
   constructor() {
     this.state = {
-      currentPage: "home",
+      currentPage: "auth",
       anyArray: [],
     };
     this.appElement = document.getElementById("app");
   }
 
   render() {
-    console.log("App is rendering");
+    let template;
+    if (this.state.currentPage === "auth") {
+      template = Handlebars.compile(Pages.AuthPage);
+      this.appElement.innerHTML = template();
+    }
   }
 }
