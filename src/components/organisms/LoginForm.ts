d178a44a1a90b6bde0746ee156ce28385c0f2b86
Sergey <yapikay@ya.ru>
2024-12-21 00:31:58 +0300

    feat: Add organism LoginForm

---
+++ src/components/organisms/LoginForm.ts
@@ -0,0 +1,96 @@
+import Block from '../../framework/Block'
+
+import { InputAuth, IInputAuth } from '@components/molecules/InputAuth'
+import { Button } from '@components/atoms/Button'
+import { Title } from '@components/atoms/Title'
+import { Link } from '@components/atoms/Link'
+
+interface InputConfig
+  extends Omit<IInputAuth, 'events' | 'value' | 'onClick'> {}
+
+const inputsConfig: InputConfig[] = [
+  {
+    type: 'text',
+    name: 'login',
+    placeholder: 'Username',
+    id: 'login',
+    label: 'Логин',
+  },
+  {
+    type: 'password',
+    name: 'password',
+    placeholder: 'Password',
+    id: 'password',
+    label: 'Пароль',
+  },
+]
+
+export class LoginForm extends Block {
+  constructor() {
+    super({
+      Inputs: inputsConfig.map(
+        (item: InputConfig) =>
+          new InputAuth({
+            label: item.label,
+            class: 'auth__formItemInput',
+            id: item.id,
+            name: item.name,
+            placeholder: item.placeholder,
+            type: item.type,
+            onClick: (event: Event) => {
+              console.log('CLICK to input ', item.id)
+              event.preventDefault()
+              event.stopPropagation()
+            },
+          })
+      ),
+      Button: new Button({
+        text: 'Войти',
+        class: 'auth__button navLink',
+        dataPage: 'chats',
+        id: 'login',
+        //TODO: добавить проверку на валидность данных
+        // и вывод объекта с данными в консоль
+        onClick: (event: Event) => {
+          console.log('CLICK to button ', event)
+          event.preventDefault()
+          event.stopPropagation()
+        },
+      }),
+      Link: new Link({
+        text: 'Нет аккаунта?',
+        href: '/register',
+        dataPage: 'register',
+        class: 'auth__link navLink',
+        onClick: (event: Event) => {
+          console.log('CLICK to link ', event)
+          event.preventDefault()
+          event.stopPropagation()
+        },
+      }),
+      Title: new Title({
+        title: 'Вход',
+        class: 'auth__title',
+      }),
+    })
+  }
+
+  render() {
+    return `
+      <main class="app">
+        <section class="auth">
+          <div class="auth__container">
+            {{{ Title }}}
+            <form class="auth__form" action="" method="get">
+              <div class="auth__formItems">
+                {{{ Inputs }}}
+              </div>
+              {{{ Button }}}
+              {{{ Link }}}
+            </form>
+          </div>
+        </section>
+      </main>
+    `
+  }
+}
